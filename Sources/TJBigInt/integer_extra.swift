9fd174b8166601d3059cc4bb6e9ca6ca77c31b51
Tjienta Vara <tjientavara@users.noreply.github.com>
2017-06-15 07:52:00 +0200

    Set dependency for TJRandom.

--- Sources/TJBigInt/integer_extra.swift
+++ Sources/TJBigInt/integer_extra.swift
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import Foundation
-import TJBigIntC
 
 public protocol PowerOfTwo {
     // Count the number bits that are set to '1'.
